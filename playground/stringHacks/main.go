02df59d8bb85a0e472f68ba4f301427ccf163d30
simorgh1 <bahram.maravandi@gmail.com>
2020-05-03 16:03:10 +0200

    invert string added

---
+++ playground/stringHacks/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "Hello World!"
+
+	fmt.Printf("Invert the ascii string: '%s'\n", str)
+
+	// string in go is immutable, convert it first to rune before inverting.
+	buf := []rune(str)
+
+	// traverse through the last char to the first and replace them
+	for i, _ := range str {
+		buf[i] = rune(str[len(str)-1-i])
+	}
+
+	// convert back to string
+	str = string(buf)
+	fmt.Printf("Result: '%s'\n", str)
+}
